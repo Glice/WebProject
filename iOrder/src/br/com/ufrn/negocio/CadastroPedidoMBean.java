de30b7cfa127d3d4e4948d52007b3f747979cbcf
Glice <glicinha.eu@gmail.com>
2017-06-26 23:08:08 -0300

    Exibindo itens em Menu.xhtml

--- iOrder/src/br/com/ufrn/negocio/CadastroPedidoMBean.java
+++ iOrder/src/br/com/ufrn/negocio/CadastroPedidoMBean.java
@@ -8,6 +8,8 @@ import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.sun.java.swing.plaf.windows.resources.windows;
+
 import br.com.ufrn.dao.GenericDAO;
 import br.com.ufrn.dao.ItemCardapioDAO;
 import br.com.ufrn.dominio.ItemCardapio;
@@ -21,22 +23,23 @@ public class CadastroPedidoMBean {
 	@Inject GenericDAO dao;
 	
 	private final String PedidoForm = "Formulario.xhtml";
+	
 	private Pedido pedido = new Pedido();
 	private ItemCardapio item = new ItemCardapio();
 	// Itens que na tela de seleção
-	private List<ItemCardapio> itemList = new ArrayList<>();
-	
-//	private List<ItemCardapio> itensSelecionados = new ArrayList<>(); // necessário?
+	private List<ItemCardapio> itensMenu = new ArrayList<>();
+	private List<String> itensSelecionados = new ArrayList<>();
 		
 
 	public CadastroPedidoMBean() {
-		
-		this.item.setId(1);
-		this.item.setNome("item teste");
-		ItemCardapio elemento = new ItemCardapio();
-		this.itemList.add(item);
-		this.itemList.add(elemento);
-		this.itemList.add(item);
+		// pegar do banco:
+		// itensMenu = itens_no_Banco
+		for (int i=1; i<=5; i++) {
+			this.item = new ItemCardapio();
+			this.item.setId(i);
+			this.item.setNome("Item" + i);
+			this.itensMenu.add(item);
+		}
 	}
 
 	public ItemCardapio getItem() {
@@ -62,20 +65,20 @@ public class CadastroPedidoMBean {
 //	}
 	
 	public void addItem() {
-		this.itemList.add(item);
+		this.itensMenu.add(item);
 		pedido.addItem(this.item);
 		this.item = null;
 	}
 	
 	public void removeItem(ItemCardapio item) {
 		pedido.removeItem(item);
-		this.itemList.remove(item);
+		this.itensMenu.remove(item);
 	}
 	
 	public void listarItens(){
 		ItemCardapioDAO itemDAO = new ItemCardapioDAO();
 		try {
-			this.setItemList(itemDAO.findAll());
+			this.setItensMenu(itemDAO.findAll());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -89,16 +92,39 @@ public class CadastroPedidoMBean {
 		return "URL_de_confirmacao";
 	}
 
-	public List<ItemCardapio> getItemList() {
+	/*public List<ItemCardapio> getItemList() {
 		return itemList;
 	}
 
 	public void setItemList(List<ItemCardapio> itemList) {
 		this.itemList = itemList;
+	}*/
+	
+	public List<ItemCardapio> getItensMenu() {
+		return itensMenu;
+	}
+
+	public void setItensMenu(List<ItemCardapio> itensMenu) {
+		this.itensMenu = itensMenu;
+	}
+
+	public List<String> getItensSelecionados() {
+		return itensSelecionados;
+	}
+
+	public void setItensSelecionados(List<String> itensSelecionados) {
+		this.itensSelecionados = itensSelecionados;
 	}
 	
-	public String criarPedido(){
-		return PedidoForm;
+	
+	public String enviarPedido(){
+		/*for (String id : itensSelecionados) {
+//			this.dao.inserir(item);
+			ItemCardapio item = itensMenu
+			pedido.addItem(item);
+		}*/
+		
+		return "Index.xhtml";
 	}
 	
 }
